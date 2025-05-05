021304fb30c60b286366806fda7d26308a0cc2de
Shixu Luo <git@luo.sx>
2025-05-05 00:08:50 -0400

    fix: add debut log

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,7 @@ import characters from '../data/characters';
 const BASE_URL = import.meta.env.BASE_URL;
 
 export default function Card({ name, onClick }: { name: string, onClick: ()=>void }) {
+  console.log(BASE_URL + characters.get(name));
   
   return (
     <div className="w-[300px] h-[500px] border border-black" onClick={onClick}>
