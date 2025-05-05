04f4353ceddd6f59e2dc0ceb93c871ff21227bba
Shixu Luo <git@luo.sx>
2025-05-04 23:33:15 -0400

    fix: use relative file path

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1,15 +1,15 @@
 const characters: Map<string, string> = new Map([
-  ['Pam', '/pam.jpg'],
-  ['Jim', '/jim.jpg'],
-  ['Michael', '/michael.jpg'],
-  ['Dwight', '/dwight.jpg'],
-  ['Jan', '/jan.jpg'],
-  ['Karen', '/karen.jpg'],
-  ['Kevin', '/kevin.jpg'],
-  ['Toby', '/toby.jpg'],
-  ['Ryan', '/ryan.jpg'],
-  ['Kelly', '/kelly.jpg'],
-  ['Creed', '/creed.jpg'],
+  ['Pam', './pam.jpg'],
+  ['Jim', './jim.jpg'],
+  ['Michael', './michael.jpg'],
+  ['Dwight', './dwight.jpg'],
+  ['Jan', './jan.jpg'],
+  ['Karen', './karen.jpg'],
+  ['Kevin', './kevin.jpg'],
+  ['Toby', './toby.jpg'],
+  ['Ryan', './ryan.jpg'],
+  ['Kelly', './kelly.jpg'],
+  ['Creed', './creed.jpg'],
 ]);
 
 export function getCharacter(): string[] {
