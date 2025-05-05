017176f8bf9105d3011ad38e008ec94887647591
Shixu Luo <git@luo.sx>
2025-05-04 23:22:15 -0400

    fix: fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/the-office-memory-card-game/',
+  base: '/The-office-memory-card-game/',
   plugins: [
     react(),
     tailwindcss(),
