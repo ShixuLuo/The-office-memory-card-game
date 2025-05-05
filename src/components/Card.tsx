78b10535fd98bc1fe0467f29f7a9c8d540ca3bc9
Shixu Luo <git@luo.sx>
2025-05-05 00:00:04 -0400

    fix: change img url

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,12 @@
 import characters from '../data/characters';
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 export default function Card({ name, onClick }: { name: string, onClick: ()=>void }) {
   
   return (
     <div className="w-[300px] h-[500px] border border-black" onClick={onClick}>
-      <img src={'/' + characters.get(name)} className="h-full w-full object-cover"/>
+      <img src={BASE_URL + characters.get(name)} className="h-full w-full object-cover"/>
     </div>
   )
 }
